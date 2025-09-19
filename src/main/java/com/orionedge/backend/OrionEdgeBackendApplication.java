b63d22c4e8b79efa5571d25cd9f17d247b5f0b5e
FedeTrabaBip <fatraba@bpba.com.ar>
2025-09-19 01:08:45 -0300

    primer commit backend

---
+++ src/main/java/com/orionedge/backend/OrionEdgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.orionedge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionEdgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrionEdgeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orionedge/backend/OrionEdgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orionedge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionEdgeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
