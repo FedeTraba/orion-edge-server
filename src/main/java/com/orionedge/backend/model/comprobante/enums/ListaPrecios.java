cfee206ad356f8a60d6314cf6622f8de5defb403
FedeTrabaBip <fatraba@bpba.com.ar>
2025-09-24 13:06:18 -0300

    enums y controller de comprobante

---
+++ src/main/java/com/orionedge/backend/model/comprobante/enums/ListaPrecios.java
@@ -0,0 +1,8 @@
+package com.orionedge.backend.model.comprobante.enums;
+
+public enum ListaPrecios {
+    GENERAL,
+    MAYORISTA,
+    MINORISTA,
+    PROMOCION
+}
---
+++ src/main/java/com/orionedge/backend/model/comprobante/enums/TipoMovimiento.java
@@ -0,0 +1,10 @@
+package com.orionedge.backend.model.comprobante.enums;
+
+public enum TipoMovimiento {
+    FACTURA,
+    PRESUPUESTO,
+    NOTA_DEBITO,
+    NOTA_CREDITO,
+    AJUSTE_DEBITO,
+    AJUSTE_CREDITO
+}
